4e4447b8e223d85892f9d99e31eecce3b7f7d29f
EC2 Default User <ec2-user@ip-172-31-9-252.ap-south-1.compute.internal>
2024-04-02 05:40:56 +0000

    new app added

---
+++ src/HelloWorld.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Paper, Typography } from '@mui/material';
+
+const HelloWorld = () => {
+  return (
+    <Container maxWidth="sm" style={{ marginTop: '50px' }}>
+      <Paper elevation={3} style={{ padding: '20px' }}>
+        <Typography variant="h4" align="center">
+          Hello, World!
+        </Typography>
+      </Paper>
+    </Container>
+  );
+};
+export default HelloWorld;
